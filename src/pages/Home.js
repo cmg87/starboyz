1020ddfdbbfa52320bdf2223e790a4fd89412285
chris <chrisgoerler@gmail.com>
2021-01-24 13:40:15 -0500

    video in ios try

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,15 +4,13 @@ import Nav from '../components/Nav'
 import Video from "../components/Video";
 import About from "../components/About";
 import Gallery from "../components/Gallery";
-import Video2 from "../components/Video2";
 
 function Home() {
     return (
         <div className='bg-black'>
             <Nav/>
             <Header/>
-            {/*<Video/>*/}
-            <Video2/>
+            <Video/>
             <About/>
             <Gallery/>
         </div>
