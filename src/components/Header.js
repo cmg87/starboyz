3d55c1de91ed27d5e48de5faafb6f5470908007d
chris <chrisgoerler@gmail.com>
2021-01-24 14:53:18 -0500

    faster image loading

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ function Header() {
     return (
         <div id={'header'}  className="bg-gray-900 w-full sm:h-screen h-60 sm:mt-12 mt-8 flex justify-center items-center opacity-50 z-20 absolute left-0">
             {/*<div className='underline text-gray-400 sm:text-9xl text-4xl text-opacity-75 font-sb'>StarBoyz</div>*/}
-            <img src={logo} className='w-1/2 h-auto' alt=""/>
+            <img src='https://video.chrisgoerler.com/starboyzlogo.png' className='w-1/2 h-auto' alt=""/>
         </div>
     );
 }
