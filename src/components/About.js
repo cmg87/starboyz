9e52701e881e9665d190caab2bfe253492c82988
chris <chrisgoerler@gmail.com>
2021-02-24 00:34:07 -0500

    spelling errors

--- src/components/About.js
+++ src/components/About.js
@@ -6,7 +6,7 @@ function About() {
         <div id="about" className='flex justify-center text-center flex-col font-sb pt-40  bg-black text-white'>
             <div className="text-4xl underline pt-3.5 pb-8"> Our Story</div>
             <div className="sm:grid-cols-4 sm:grid">
-                <div className="sm:text-2xl pt-3.5 sm:col-start-2 sm:col-end-4 clear-both p-10 sm:p-0 mb-16"> After all the years of friendship, adventure and even hardships... Starboyz stood the Test of time... We realized the last few years while taking a break, that what we started was an idea, a lifestyle that was bigger than us... It wasn’t about us anymore, it was about the riders that helped us succeed, the friends we made along the way, and the culture that we bred... We realized the last few years that our bonds and friendships were stronger than ever, they were never torn, we did not abandon one another, nor did we abandon our ideas, our principles, or our sense of community... Riding motorcycles brought us together and that bond is engrained in our soul, it flows in our veins... So we put this site together for you all to enjoy and keep the legacy of our accomplishments and the impact we made on the industry out there. Enjoy...
+                <div className="sm:text-2xl pt-3.5 sm:col-start-2 sm:col-end-4 clear-both p-10 sm:p-0 mb-16"> After all the years of friendship, adventure and even hardships... StarBoyz stood the test of time... We realized the last few years while taking a break, that what we started was an idea, a lifestyle that was bigger than us... It wasn't about us anymore, it was about the riders that helped us succeed, the friends we made along the way, and the culture that we bred... We realized the last few years that our bonds and friendships were stronger than ever, they were never torn, we did not abandon one another, nor did we abandon our ideas, our principles, or our sense of community... Riding motorcycles brought us together and that bond is engrained in our soul, it flows in our veins... So we put this site together for you all to enjoy and keep the legacy of our accomplishments and the impact we made on the industry out there. Enjoy...
 
                     Side note: FTP
 
@@ -17,7 +17,7 @@ function About() {
                         Brandon Nutter,
                         Josh Conway,
                         Jeff “Cbear” Flaherty,
-                        Brandon Carl</p>
+                        Brandon Carl, Johnny Hayes</p>
                 </div>
 
             </div>
